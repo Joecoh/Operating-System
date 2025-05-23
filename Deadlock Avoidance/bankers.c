8c3aa4805c5ad4be62a8e2c8d27cdcbb6547d624
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-05-23 21:21:45 +0530

    Create bankers.c

---
+++ Deadlock Avoidance/bankers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t mutex;
+
+void* thread(void* arg) {
+    // Wait
+    sem_wait(&mutex);
+    printf("\nEntered..\n");
+
+    // Critical section
+    sleep(4);
+
+    printf("\nJust Exiting...\n");
+
+    // Signal
+    sem_post(&mutex);
+    return NULL;
+}
+
+int main() {
+    sem_init(&mutex, 0, 1); // 0 = thread sharing
+
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, thread, NULL);
+    sleep(2);  // Ensures t1 locks first
+    pthread_create(&t2, NULL, thread, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    sem_destroy(&mutex);
+    return 0;
+}
