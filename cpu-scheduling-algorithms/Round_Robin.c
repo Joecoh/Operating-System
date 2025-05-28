855dcb299c63d800e4f5c99d6b4c0ec78ddc073c
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-05-28 19:20:37 +0530

    Update Round_Robin.c

--- cpu-scheduling-algorithms/Round_Robin.c
+++ cpu-scheduling-algorithms/Round_Robin.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
-#include <semaphore.h>
-#include <pthread.h>
-
-sem_t sem;
-
-void* thread_func(void* arg) {
-    printf("Thread waiting on semaphore...\n");
-    sem_wait(&sem);         
-    printf("Thread inside critical section!\n");
-    sem_post(&sem);          
-    return NULL;
-}
 
 int main() {
-    pthread_t t;
+    int n, i, time_quantum;
+    int bt[20], rem_bt[20], wt[20], tat[20];
+    int t = 0, done;
 
-    sem_init(&sem, 0, 1);   
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
 
-    pthread_create(&t, NULL, thread_func, NULL);
+    printf("Enter burst time for each process:\n");
+    for(i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+        rem_bt[i] = bt[i];  // initialize remaining burst time
+        wt[i] = 0;          // initialize waiting time
+    }
 
-    sem_wait(&sem);
-    printf("Main thread inside critical section!\n");
-    sem_post(&sem);
+    printf("Enter time quantum: ");
+    scanf("%d", &time_quantum);
 
-    pthread_join(t, NULL);
-    sem_destroy(&sem);      
+    while(1) {
+        done = 1;
+        for(i = 0; i < n; i++) {
+            if(rem_bt[i] > 0) {
+                done = 0;  // there is a pending process
+
+                if(rem_bt[i] > time_quantum) {
+                    t += time_quantum;
+                    rem_bt[i] -= time_quantum;
+                } else {
+                    t += rem_bt[i];
+                    wt[i] = t - bt[i];  // waiting time = current time - burst time
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+        if(done)
+            break;
+    }
+
+    int total_wt = 0, total_tat = 0;
+    printf("P#\tBurst\tWait\tTurnaround\n");
+    for(i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t%d\t%d\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+    }
+
+    printf("Average Waiting Time = %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time = %.2f\n", (float)total_tat / n);
 
     return 0;
 }
