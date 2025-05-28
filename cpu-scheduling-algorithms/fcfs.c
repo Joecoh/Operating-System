a59503b4a626839e1dfa40de2ba00db1d182ae8a
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-05-28 19:14:53 +0530

    Update fcfs.c

--- cpu-scheduling-algorithms/fcfs.c
+++ cpu-scheduling-algorithms/fcfs.c
@@ -1,73 +1,31 @@
-#include <stdio.h>
+echo "Enter number of processes:"
+read n
 
-struct process {
-    int pid;
-    int btime;
-    int wtime;
-    int ttime;
-} p[10];
+declare -a bt wait tat
+total_wt=0
+total_tat=0
 
-int main() {
-    int i, j, k, n, ttur, twat;
-    float awat, atur;
+for (( i=0; i<n; i++ ))
+do
+    echo "Enter burst time for process $((i+1)):"
+    read bt[i]
+done
 
-    printf("Enter no. of process : ");
-    scanf("%d", &n);
+wait[0]=0
 
-    for(i = 0; i < n; i++) {
-        printf("Burst time for process P%d (in ms) : ", (i + 1));
-        scanf("%d", &p[i].btime);
-        p[i].pid = i + 1;
-    }
+for (( i=1; i<n; i++ ))
+do
+    wait[i]=$(( wait[i-1] + bt[i-1] ))
+done
 
-    p[0].wtime = 0;
-    for(i = 1; i < n; i++) {
-        p[i].wtime = p[i - 1].wtime + p[i - 1].btime;
-    }
+echo "P#  Burst  Wait  Turnaround"
+for (( i=0; i<n; i++ ))
+do
+    tat[i]=$(( bt[i] + wait[i] ))
+    total_wt=$(( total_wt + wait[i] ))
+    total_tat=$(( total_tat + tat[i] ))
+    echo "$((i+1))   ${bt[i]}     ${wait[i]}      ${tat[i]}"
+done
 
-    for(i = 0; i < n; i++) {
-        p[i].ttime = p[i].wtime + p[i].btime;
-    }
-
-    ttur = twat = 0;
-    for(i = 0; i < n; i++) {
-        ttur += p[i].ttime;
-        twat += p[i].wtime;
-    }
-
-    awat = (float)twat / n;
-    atur = (float)ttur / n;
-
-    printf("\nFCFS Scheduling\n\n");
-    for(i = 0; i < 28; i++) printf("-");
-    printf("\nProcess B-Time T-Time W-Time\n");
-    for(i = 0; i < 28; i++) printf("-");
-    for(i = 0; i < n; i++) {
-        printf("\nP%d\t%4d\t%3d\t%2d", p[i].pid, p[i].btime, p[i].ttime, p[i].wtime);
-    }
-    printf("\n");
-    for(i = 0; i < 28; i++) printf("-");
-    printf("\n\nAverage waiting time    : %5.2fms", awat);
-    printf("\nAverage turn around time: %5.2fms\n", atur);
-
-    printf("\n\nGANTT Chart\n");
-    for(i = 0; i < (p[n-1].ttime + 2 * n); i++) printf("-");
-    printf("\n|");
-    for(i = 0; i < n; i++) {
-        k = p[i].btime / 2;
-        for(j = 0; j < k; j++) printf(" ");
-        printf("P%d", p[i].pid);
-        for(j = k + 1; j < p[i].btime; j++) printf(" ");
-        printf("|");
-    }
-    printf("\n");
-    for(i = 0; i < (p[n-1].ttime + 2 * n); i++) printf("-");
-    printf("\n0");
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < p[i].btime; j++) printf(" ");
-        printf("%2d", p[i].ttime);
-    }
-    printf("\n");
-
-    return 0;
-}
+echo "Average Waiting Time: $(echo "scale=2; $total_wt/$n" | bc)"
+echo "Average Turnaround Time: $(echo "scale=2; $total_tat/$n" | bc)"
