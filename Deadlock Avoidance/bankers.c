d087397f029966d196fc8ca21b9b67566c8ea6bb
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-05-28 19:24:14 +0530

    Update bankers.c

--- Deadlock Avoidance/bankers.c
+++ Deadlock Avoidance/bankers.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <unistd.h>
-
-sem_t mutex;
-
-void* thread(void* arg) {
-    // Wait
-    sem_wait(&mutex);
-    printf("\nEntered..\n");
-
-    // Critical section
-    sleep(4);
-
-    printf("\nJust Exiting...\n");
-
-    // Signal
-    sem_post(&mutex);
-    return NULL;
-}
 
 int main() {
-    sem_init(&mutex, 0, 1); // 0 = thread sharing
+    int n = 3, m = 3; // processes and resources
+    int alloc[3][3] = {{0, 1, 0}, {2, 0, 0}, {3, 0, 2}};
+    int max[3][3]   = {{7, 5, 3}, {3, 2, 2}, {9, 0, 2}};
+    int avail[3]    = {3, 3, 2};
 
-    pthread_t t1, t2;
+    int need[3][3], finish[3] = {0}, safe[3], i, j, k;
 
-    pthread_create(&t1, NULL, thread, NULL);
-    sleep(2);  // Ensures t1 locks first
-    pthread_create(&t2, NULL, thread, NULL);
+    // Calculate need matrix
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
 
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    int count = 0;
+    while (count < n) {
+        for (i = 0; i < n; i++) {
+            if (!finish[i]) {
+                int can_run = 1;
+                for (j = 0; j < m; j++)
+                    if (need[i][j] > avail[j]) {
+                        can_run = 0;
+                        break;
+                    }
 
-    sem_destroy(&mutex);
+                if (can_run) {
+                    for (k = 0; k < m; k++)
+                        avail[k] += alloc[i][k];
+                    safe[count++] = i;
+                    finish[i] = 1;
+                }
+            }
+        }
+    }
+
+    printf("Safe sequence: ");
+    for (i = 0; i < n; i++)
+        printf("P%d ", safe[i]);
+    printf("\n");
+
     return 0;
 }
