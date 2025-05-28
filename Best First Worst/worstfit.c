2765754fc06d90de707aaf5e3473a6dcae361d97
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-05-28 19:25:57 +0530

    Create worstfit.c

---
+++ Best First Worst/worstfit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int blockSize[] = {100, 500, 200, 300, 600};
+    int processSize[] = {212, 417, 112, 426};
+    int m = 5, n = 4, i, j, worst;
+    int allocated[n];
+
+    for(i = 0; i < n; i++) {
+        worst = -1;
+        for(j = 0; j < m; j++) {
+            if(blockSize[j] >= processSize[i]) {
+                if(worst == -1 || blockSize[j] > blockSize[worst])
+                    worst = j;
+            }
+        }
+
+        if(worst != -1) {
+            allocated[i] = worst;
+            blockSize[worst] -= processSize[i];
+        } else {
+            allocated[i] = -1;
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        printf("Process %d -> ", i+1);
+        if(allocated[i] != -1)
+            printf("Block %d\n", allocated[i]+1);
+        else
+            printf("Not Allocated\n");
+    }
+
+    return 0;
+}
