422cf0c526580eb72915ea6d52349a02a4031805
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-05-28 19:18:18 +0530

    Update sjf.c

--- cpu-scheduling-algorithms/sjf.c
+++ cpu-scheduling-algorithms/sjf.c
@@ -1,84 +1,50 @@
 #include <stdio.h>
 
-struct process {
-    int pid;
-    int btime;
-    int wtime;
-    int ttime;
-} p[10], temp;
-
 int main() {
-    int i, j, k, n, ttur, twat;
-    float awat, atur;
+    int n, i, j, pos, temp;
+    int bt[20], wt[20], tat[20], p[20];
+    int total_wt = 0, total_tat = 0;
 
-    printf("Enter no. of process : ");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
+    printf("Enter burst time for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("Burst time for process P%d (in ms) : ", (i + 1));
-        scanf("%d", &p[i].btime);
-        p[i].pid = i + 1;
+        printf("Process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+        p[i] = i + 1;  // store process number
     }
 
-    for(i = 0; i < n - 1; i++) {
+    // Sort processes by burst time (SJF)
+    for(i = 0; i < n-1; i++) {
+        pos = i;
         for(j = i + 1; j < n; j++) {
-            if((p[i].btime > p[j].btime) || 
-               (p[i].btime == p[j].btime && p[i].pid > p[j].pid)) {
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
+            if(bt[j] < bt[pos])
+                pos = j;
         }
+        // swap burst time
+        temp = bt[i]; bt[i] = bt[pos]; bt[pos] = temp;
+        // swap process number
+        temp = p[i]; p[i] = p[pos]; p[pos] = temp;
     }
 
-    p[0].wtime = 0;
+    wt[0] = 0; // waiting time of first process is zero
+
+    // calculate waiting time
     for(i = 1; i < n; i++) {
-        p[i].wtime = p[i - 1].wtime + p[i - 1].btime;
+        wt[i] = wt[i - 1] + bt[i - 1];
     }
 
+    printf("P#\tBurst\tWait\tTurnaround\n");
     for(i = 0; i < n; i++) {
-        p[i].ttime = p[i].wtime + p[i].btime;
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t%d\t%d\t%d\n", p[i], bt[i], wt[i], tat[i]);
     }
 
-    ttur = twat = 0;
-    for(i = 0; i < n; i++) {
-        ttur += p[i].ttime;
-        twat += p[i].wtime;
-    }
-
-    awat = (float)twat / n;
-    atur = (float)ttur / n;
-
-    printf("\nSJF Scheduling\n\n");
-    for(i = 0; i < 28; i++) printf("-");
-    printf("\nProcess B-Time T-Time W-Time\n");
-    for(i = 0; i < 28; i++) printf("-");
-    for(i = 0; i < n; i++) {
-        printf("\nP%-4d\t%4d\t%3d\t%2d", p[i].pid, p[i].btime, p[i].ttime, p[i].wtime);
-    }
-    printf("\n");
-    for(i = 0; i < 28; i++) printf("-");
-    printf("\n\nAverage waiting time    : %5.2fms", awat);
-    printf("\nAverage turn around time: %5.2fms\n", atur);
-
-    printf("\n\nGANTT Chart\n");
-    for(i = 0; i < (p[n - 1].ttime + 2 * n); i++) printf("-");
-    printf("\n|");
-    for(i = 0; i < n; i++) {
-        k = p[i].btime / 2;
-        for(j = 0; j < k; j++) printf(" ");
-        printf("P%d", p[i].pid);
-        for(j = k + 1; j < p[i].btime; j++) printf(" ");
-        printf("|");
-    }
-    printf("\n");
-    for(i = 0; i < (p[n - 1].ttime + 2 * n); i++) printf("-");
-    printf("\n0");
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < p[i].btime; j++) printf(" ");
-        printf("%2d", p[i].ttime);
-    }
-    printf("\n");
+    printf("Average Waiting Time = %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time = %.2f\n", (float)total_tat / n);
 
     return 0;
 }
